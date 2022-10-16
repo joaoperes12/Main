e91c898dced1a23eed0a485f074ed9d2a6abade2
ironblodz <iamjoaoperes19@outlook.pt>
2022-10-16 01:59:19 +0100

    update main

--- resources/views/categories/partials/add-edit.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="form-group">
-    <label for="inputName">Name</label>
-    <input type="text" class="form-control" name="name" id="inputName" value="{{old('name',$category->name)}}" />
-</div>
-<div class="form-group">
-    <label for="inputDescription">Descrição</label>
-    <textarea class="form-control" name="content" id="inputDescription">{{old('content',$category->content)}}</textarea>
-</div>
-
--- resources/views/partials/errors.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="alert alert-danger">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <ul>
-    @foreach ($errors ->all() as $message)
-        <li>{{$message}}</li>
-    @endforeach
-    </ul>
-</div>
